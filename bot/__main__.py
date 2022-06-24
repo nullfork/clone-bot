01869fdc39fff04b8342dc78732be3c7334b7990
Daniel <61970262+rozari0@users.noreply.github.com>
2022-06-24 10:09:55 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -109,9 +109,9 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton(
-        "Repo", "https://www.github.com/yashoswalyo/clone-bot-public"
+        "Channel", "https://t.me/bang_mirror"
     )
-    buttons.buildbutton("Report Group", "https://t.me/yash_codes_support")
+    buttons.buildbutton("Rocket", "https://t.me/TheCrowClub")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f"""
